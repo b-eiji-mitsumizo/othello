10571a231deffafc0726f1afcea283136a1f3c1d
b-eiji-mitsumizo <eiji.mitsumizo@mail.benesse.co.jp>
2021-05-14 10:55:03 +0900

    交互にプレイ

--- Main.java
+++ Main.java
@@ -7,10 +7,22 @@ public class Main {
 		
 		Board board = new Board();
 		
-		Player player1 = new Player();
-		Player player2 = new Player();
+		Player player1 = new Player("EIJI", "●");
+		Player player2 = new Player("MIKI", "○");
 		
-		board.displayBoard();
+		
+
+		int count = 0;
+		boolean finish = false;
+		
+		while(count < 9) {
+			board.displayBoard();
+			board.checkIfAddKoma(count % 2 == 0 ? player1 : player2);
+			if(finish) {
+				break;
+			}
+			count++;
+		}
 		}
 
 }
